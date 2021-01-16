a1f82f56acdc143142dbdab5f14a3fd7b215cb69
Million900o <apaolini900o@gmail.com>
2021-01-16 02:29:55 -0500

    bruh

--- index.js
+++ index.js
@@ -1,244 +1,180 @@
-//dependencies
-const electron = require('electron')
-const { app, Menu, Tray, BrowserWindow, globalShortcut, clipboard, ipcMain, nativeImage, Notification } = require('electron')
+// Dependencies
+const { app, Menu, Tray, BrowserWindow, globalShortcut, clipboard, ipcMain, nativeImage, Notification } = require('electron');
 const settings = require('electron-settings');
-const path = require('path')
-const fs = require('fs')
-const request = require('request')
 const activeWin = require('active-win');
-const isJson = require('is-json')
-const shell = require('shelljs')
-shell.config.execPath = shell.which('node')
+const FormData = require('form-data');
+const fetch = require('node-fetch');
+const shell = require('shelljs');
+const path = require('path');
+const fs = require('fs');
+
+// Functions
+const createFileName = require('./functions/createFileName');
+
+// Set node
+shell.config.execPath = shell.which('node');
 
 //variables
-const icon = path.join(__dirname, 'assets/icons/icon.png')
-let tray = null
-let win = null
-let settingsWin = null
-let notif_clip = null
-let notif_upload = null
-let notif_error = null
-let temp_img_path = path.join(app.getPath('home') + '/screenshot.png')
-//commands
-let fullscreen_cmd = 'screencapture ' + temp_img_path
-let selection_cmd = 'screencapture -i ' + temp_img_path
-let window_cmd = 'screencapture -l$id ' + temp_img_path
+let settingsWin = null;
+let tray = null;
 
-//ready
-app.on('ready', (event) => {
-  app.dock.hide();
-  //settings
-  if (!settings.has('config')) {
-    settings.set('config', {
-      'save': 'imgur',
-      'c_fs': 'Command+7',
-      'c_s': 'Command+8',
-      'c_w': 'Command+9'
-    })
-  }
-  let conf = settings.get('config')
-  //load notifications
-  notif_clip = new Notification({
-    title: 'Screenshot Taken',
-    body: 'Screenshot has been copied to your clipboard'
-  })
+// Load notifications
+const notif_clip = () => new Notification({ title: 'Screenshot Taken', body: 'Screenshot has been copied to your clipboard' }).show();
+const notif_upload = () => new Notification({ title: 'Screenshot Uploaded', body: 'Screenshot link has been copied to your clipboard' }).show();
+const notif_saved = () => new Notification({ title: 'Screenshot Uploaded', body: 'Screenshot has been saved to file' }).show();
+const customError = (t) => new Notification({ title: 'Error', body: t }).show();
 
-  notif_upload = new Notification({
-    title: 'Screenshot Uploaded',
-    body: 'Screenshot link has been copied to your clipboard'
-  })
-
-  notif_error = new Notification({
-    title: 'Error',
-    body: 'Something went wrong while uploading'
-  })
-
-
-  //tray
-  tray = new Tray(icon)
-
-  //tray menu
-  const contextMenu = trayMenu(conf)
-
-  tray.setToolTip('ScreenShotX')
-  tray.setContextMenu(contextMenu)
-
-
-  const menu = Menu.buildFromTemplate(template)
-  Menu.setApplicationMenu(menu)
-  //Shortcuts
-  globalShortcut.register(conf.c_fs, () => {
-    let child = shell.exec(fullscreen_cmd, { async: true })
-  })
-  globalShortcut.register(conf.c_s, async () => {
-    let child = shell.exec(selection_cmd, { async: true })
-  })
-  globalShortcut.register(conf.c_w, async () => {
-    let active = await activeWin()
-    if (!active) return
-
-    shell.exec(window_cmd.replace('$id', active.id), { async: true })
-  })
-
-})
-
-app.on('window-all-closed', (event) => {
-  event.preventDefault()
-  app.dock.hide();
-})
-
-ipcMain.on('settings:update', async (e, item) => {
-  let conf = item
-  globalShortcut.unregisterAll()
-  globalShortcut.register(conf.c_fs, () => {
-    let child = shell.exec(fullscreen_cmd, { async: true })
-  })
-  globalShortcut.register(conf.c_s, () => {
-    let child = shell.exec(selection_cmd, { async: true })
-  })
-  globalShortcut.register(conf.c_w, async () => {
-    let active = await activeWin()
-    if (!active) return
-
-    shell.exec(window_cmd.replace('$id', active.id), { async: true })
-  })
-  settings.set('config', item)
-})
-
-//notification types
-
-
-
-
-
-
-let file = `${app.getPath('home')}/screenshot.png`;
-function watchFile() {
-
-  let exist = fs.existsSync(file)
-  if (!exist) {
-    setTimeout(() => {
-      watchFile()
-    }, 1000)
-    return
-  }
-  let date = new Date()
-  let filename = `Screen Shot ${date.getDate()}-${date.getMonth()}-${date.getFullYear()} (${date.getHours()}.${date.getMinutes()}.${date.getSeconds()}).png`
-  let conf = settings.get('config')
-
-  if (conf.save === 'clipboard') {
-    let image = nativeImage.createFromPath(`${app.getPath('home')}/screenshot.png`)
-    clipboard.writeImage(image)
-    notif_clip.show()
-    fs.unlinkSync(file)
-    watchFile()
-    return
-  }
-  fs.readFile(file, function read(err, data) {
-    if (conf.save === 'imgur') {
-      let req = request.post({ url: 'https://api.imgur.com/3/upload', qs: { name: filename, image: data.toString('base64') }, headers: { 'Authorization': 'Client-ID 6a5400948b3b376' } }, function (err, resp, body) {
-        if (err) return console.error(err)
-        if (!isJson(body)) return notif_error.show()
-        let json = JSON.parse(body)
-        if (conf.open === true) require("electron").shell.openExternal(json.data.link);
-        clipboard.writeText(json.data.link)
-        notif_upload.show()
-      });
-    } else if (conf.save === "local") {
-      let file_path = `${app.getPath('documents')}/screenshots/` + filename
-      if (!fs.existsSync(`${app.getPath('documents')}/screenshots`)) {
-        fs.mkdirSync(`${app.getPath('documents')}/screenshots`)
-      }
-      fs.writeFile(file_path, data, function (err) {
-        if (err) console.log(err)
-      })
-    } else if (conf.save === "custom") {
-      let header = {}
-      header[conf['custom_settings']['key']] = conf['custom_settings']['auth']
-
-      let req = request.post({ url: conf['custom_settings']['url'], headers: header }, function (err, resp, body) {
-        if (err) return console.error(err)
-        if (!isJson(body)) return notif_error.show()
-        let json = JSON.parse(body)
-        if (json.error) return
-        if (conf.open === true) require("electron").shell.openExternal(json[conf['custom_settings']['rurl']]);
-        clipboard.writeText(json[conf['custom_settings']['rurl']])
-        notif_upload.show()
-      });
-      let form = req.form()
-      form.append('file', data, {
-        filename: filename,
-        type: 'image/png'
-      })
-    }
-  });
-  setTimeout(() => {
-    fs.unlinkSync(file)
-    watchFile()
-  }, 1000)
+// Commands
+// Get tmp file path
+let temp_img_path = path.join(app.getPath('home') + '/screenshot.png');
+// Create commands as variables
+let fullscreen_cmd = 'screencapture ' + temp_img_path;
+let selection_cmd = 'screencapture -i ' + temp_img_path;
+let window_cmd = 'screencapture -l$id ' + temp_img_path;
+// Create functions for easy access
+function screenshotFullscreen() {
+  // Run fullscreen command
+  shell.exec(fullscreen_cmd, { async: true });
+}
+function screenshotSelection() {
+  // Run selection command
+  shell.exec(selection_cmd, { async: true });
+}
+async function screenshotWindow() {
+  // Get active window
+  let active = await activeWin();
+  // Return if no active window
+  if (!active) return customError('No active window found');
+  // Otherwise, run he command with the correct ID
+  else shell.exec(window_cmd.replace('$id', active.id), { async: true });
 }
 
+// Ready
+app.on('ready', (event) => {
+  // Set Settings
+  if (!settings.has('config')) {
+    settings.set('config', {
+      'save': 'local',
+      'c_fs': 'Command+7',
+      'c_s': 'Command+8',
+      'c_w': 'Command+9',
+      'o_se': 'Command+Shift+i',
+    });
+    openSettings();
+  } else app.dock.hide();
 
-watchFile()
+  let conf = settings.get('config');
 
+  // Tray
+  tray = new Tray(path.join(__dirname, 'assets/icons/icon.png'));
+  tray.setToolTip('ScreenShotX');
+  tray.setContextMenu(trayMenu(conf));
+  Menu.setApplicationMenu(Menu.buildFromTemplate(template));
 
+  // Shortcuts
+  registerShortcuts(conf);
+  watchFile();
+});
+
+// When window closes
+app.on('window-all-closed', (event) => {
+  event.preventDefault();
+  app.dock.hide();
+});
+
+// When settings update
+ipcMain.on('settings:update', async (e, item) => {
+  registerShortcuts(item);
+  settings.set('config', item);
+});
+
+let file = `${app.getPath('home')}/screenshot.png`;
+
+// Function to watch the file
+function watchFile() {
+  if (!fs.existsSync(file)) return setTimeout(() => { watchFile(); }, 1000);
+
+  const filename = createFileName(new Date());
+  let conf = settings.get('config');
+
+  if (conf.save === 'clipboard') {
+    let image = nativeImage.createFromPath(`${app.getPath('home')}/screenshot.png`);
+    clipboard.writeImage(image);
+    notif_clip();
+  } else fs.readFile(file, (err, data) => {
+    if (err) return customError(err.toString());
+    if (conf.save === "local") {
+      let file_path = `${app.getPath('documents')}/screenshots/` + filename;
+      if (!fs.existsSync(`${app.getPath('documents')}/screenshots`)) {
+        fs.mkdirSync(`${app.getPath('documents')}/screenshots`);
+      }
+      fs.writeFile(file_path, data, (err) => {
+        if (err) console.log(err);
+      });
+      notif_saved();
+    } else if (conf.save === "custom") {
+      const formData = new FormData();
+      formData.append('file', data, 'image/png');
+
+      let headers = formData.getHeaders();
+      headers[conf['custom_settings']['key']] = conf['custom_settings']['auth'];
+
+      fetch(conf['custom_settings']['url'], {
+        method: 'POST',
+        headers: headers,
+        body: formData
+      }).then(async (res) => {
+        const text = await res.text();
+        const rurl = conf['custom_settings']['rurl'].toString();
+        if (rurl === 'response') {
+          clipboard.writeText(text);
+          notif_upload();
+        } else if (rurl.startsWith('json.')) {
+          rurl.replace('json.', '');
+          let json;
+          try {
+            json = JSON.parse(text);
+          } catch (err) {
+            customError(err.toString());
+          }
+          const args = rurl.split('.');
+          const url = args.reduce((T, A) => (T = (json[A] || {}), T), null);
+          clipboard.writeText(url);
+          notif_upload();
+        } else {
+          customError('Response URL not acceptable');
+        }
+      }).catch(e => {
+        customError(e.toString());
+      });
+    } else customError('Save method is not accepted yet.');
+  });
+  setTimeout(() => { fs.unlinkSync(file); watchFile(); }, 1000);
+}
+
+// Function to create the menu
 function trayMenu(config) {
   return Menu.buildFromTemplate([
     {
       label: 'Capture Full Screen',
       accelerator: config.c_fs,
-      click: function () {
-        shell.exec(selection_cmd, { async: true })
-      }
+      click: screenshotFullscreen
     },
     {
       label: 'Capture Selection',
       accelerator: config.c_s,
-      click: function () {
-        shell.exec(selection_cmd, { async: true })
-      }
+      click: screenshotSelection
     },
     {
       label: 'Capture Window',
       accelerator: config.c_w,
-      click: async () => {
-        let active = await activeWin()
-        if (!active) return
-
-        shell.exec(window_cmd.replace('$id', active.id), { async: true })
-      }
+      click: screenshotWindow
     },
     { type: 'separator' },
     {
       label: 'Settings',
-      click: async () => {
-        if (settingsWin) return settingsWin.moveTop()
-        let x = 0;
-        settingsWin = new BrowserWindow({
-          show: false,
-          title: "ScreenShotX",
-          width: 600,
-          height: 500,
-          resizeable: false,
-          maximizable: false,
-          webPreferences: {
-            nodeIntegration: true
-          }
-        })
-        settingsWin.loadURL(`file://${__dirname}/static/settings.html`)
-
-        settingsWin.webContents.on('dom-ready', () => {
-          console.log('yes')
-          let conf = settings.get('config')
-          app.dock.show();
-          settingsWin.webContents.send('settings:start', conf);
-          settingsWin.moveTop()
-        });
-        //events
-        settingsWin.on('closed', function () {
-          settingsWin = null
-        })
-      }
+      accelerator: config.o_se,
+      click: openSettings
     },
     { type: 'separator' },
     {
@@ -246,7 +182,42 @@ function trayMenu(config) {
       accelerator: 'Command+Q',
       selector: 'terminate:'
     }
-  ])
+  ]);
+}
+
+// Open settings
+async function openSettings() {
+  if (settingsWin) return settingsWin.moveTop();
+  settingsWin = new BrowserWindow({
+    show: false,
+    title: "ScreenShotX",
+    width: 600,
+    height: 500,
+    resizeable: false,
+    maximizable: false,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+  settingsWin.loadURL(`file://${__dirname}/static/settings.html`);
+
+  settingsWin.webContents.on('dom-ready', () => {
+    settingsWin.webContents.send('settings:start', settings.get('config'));
+    settingsWin.moveTop();
+    app.dock.show();
+  });
+
+  settingsWin.on('closed', () => {
+    settingsWin = null;
+  });
+}
+
+// Register shortcuts
+function registerShortcuts(conf) {
+  globalShortcut.register(conf.c_fs, screenshotFullscreen);
+  globalShortcut.register(conf.c_s, screenshotSelection);
+  globalShortcut.register(conf.c_w, screenshotWindow);
+  globalShortcut.register(conf.o_se, openSettings);
 }
 
 //menu
@@ -286,7 +257,7 @@ const template = [
       { role: 'close' }
     ]
   }
-]
+];
 
 if (process.platform === 'darwin') {
   template.unshift({
@@ -302,7 +273,7 @@ if (process.platform === 'darwin') {
       { type: 'separator' },
       { role: 'quit' }
     ]
-  })
+  });
 
   // Edit menu
   template[1].submenu.push(
@@ -314,7 +285,7 @@ if (process.platform === 'darwin') {
         { role: 'stopspeaking' }
       ]
     }
-  )
+  );
 
   // Window menu
   template[3].submenu = [
@@ -323,5 +294,5 @@ if (process.platform === 'darwin') {
     { role: 'zoom' },
     { type: 'separator' },
     { role: 'front' }
-  ]
+  ];
 }
--- static/settings.js
+++ static/settings.js
@@ -1,54 +1,60 @@
-const electron = require('electron')
-const {ipcRenderer} = electron
+const electron = require('electron');
+const { ipcRenderer } = electron;
 
-document.getElementById('update-btn').onclick = function() {
+function wait(a) { return new Promise(r => setTimeout(() => r(), a)); }
 
-let config = {
-  c_fs: document.getElementById('c_fs').value,
-  c_s: document.getElementById('c_s').value,
-  c_w: document.getElementById('c_w').value,
-  save: document.getElementById('save-to').value,
-  custom_settings: {
-    url: document.querySelector('#custom_url').value,
-    key: document.querySelector('#custom_key').value,
-    auth: document.querySelector('#custom_auth').value,
-    rurl: document.querySelector('#custom_rurl').value
+async function updateSettings() {
+  let config = {
+    c_fs: document.getElementById('c_fs').value,
+    c_s: document.getElementById('c_s').value,
+    c_w: document.getElementById('c_w').value,
+    o_se: document.getElementById('o_se').value,
+    save: document.getElementById('save-to').value,
+    custom_settings: {
+      url: document.querySelector('#custom_url').value,
+      key: document.querySelector('#custom_key').value,
+      auth: document.querySelector('#custom_auth').value,
+      rurl: document.querySelector('#custom_rurl').value,
     }
-}
+  };
 
-ipcRenderer.send('settings:update', config);
-document.getElementById('updated').innerHTML = '<div class="alert alert-success alert-dismissible"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>Updated\</div>'
-}
+  ipcRenderer.send('settings:update', config);
+  document.getElementById('updated').innerHTML = '<div class="alert alert-success alert-dismissible"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>Updated\</div>';
+  await wait(2400);
+  document.getElementById('updated').innerHTML = '';
+};
 
-document.getElementById('save-to').onchange = function() {
-  let val = document.getElementById('save-to').value
+document.getElementById('update-btn').onclick = updateSettings;
+
+document.getElementById('save-to').onchange = () => {
+  let val = document.getElementById('save-to').value;
   if (val === 'custom') {
-    document.getElementById('custom_settings').style.visibility = "visible"
-    document.getElementById('custom_settings').style.display = "block"
-    document.getElementById('open-con').style.display = "block"
-    document.getElementById('open-con').style.visibility = "visible"
+    document.getElementById('custom_settings').style.visibility = "visible";
+    document.getElementById('custom_settings').style.display = "block";
   } else {
-    document.getElementById('custom_settings').style.visibility = "visible"
-    document.getElementById('custom_settings').style.display = "none"
+    document.getElementById('custom_settings').style.visibility = "hidden";
+    document.getElementById('custom_settings').style.display = "none";
   }
-}
+};
 
-ipcRenderer.on('settings:start', function(e, item) {
-  document.getElementById('container').style.visibility = "visible"
-  document.getElementById('c_fs').value = item.c_fs
-  document.getElementById('c_s').value = item.c_s
-  document.getElementById('c_w').value = item.c_w
+ipcRenderer.on('log', (e, ...args) => console.log(...args));
+
+ipcRenderer.on('settings:start', (e, item) => {
+  document.getElementById('container').style.visibility = "visible";
+  document.getElementById('c_fs').value = item.c_fs;
+  document.getElementById('c_s').value = item.c_s;
+  document.getElementById('c_w').value = item.c_w;
+  document.getElementById('o_se').value = item.o_se;
   var theValue = $('#save-to').val();
   $('option[value=' + item.save + ']')
-  .attr('selected',true);
-
+    .attr('selected', true);
   if (document.getElementById('save-to').value === 'custom') {
-    document.getElementById('custom_settings').style.visibility = "visible"
-    document.getElementById('custom_settings').style.display = "block"
-
-    document.getElementById('custom_url').value = item.custom_settings.url
-    document.getElementById('custom_key').value = item.custom_settings.key
-    document.getElementById('custom_auth').value = item.custom_settings.auth
-    document.getElementById('custom_rurl').value = item.custom_settings.rurl
+    document.getElementById('custom_settings').style.visibility = "visible";
+    document.getElementById('custom_settings').style.display = "block";
   }
-})
+  const cs = item.custom_settings || {};
+  document.getElementById('custom_url').value = cs.url || '';
+  document.getElementById('custom_key').value = cs.key || '';
+  document.getElementById('custom_auth').value = cs.auth || '';
+  document.getElementById('custom_rurl').value = cs.rurl || '';
+});
